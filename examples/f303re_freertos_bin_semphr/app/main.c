73fe4d69d505589fa26c97bd480accbcdcde5270
dflubacher <98175786+dflubacher@users.noreply.github.com>
2022-05-15 09:29:44 +0000

    Binary Semaphore example with TaskDelayUntil.

--- examples/f303re_freertos_bin_semphr/app/main.c
+++ examples/f303re_freertos_bin_semphr/app/main.c
@@ -13,6 +13,7 @@
 
 // Device header.
 #include "portmacro.h"
+#include "projdefs.h"
 #include "stm32f3xx.h"
 
 // BSP functions.
@@ -42,7 +43,9 @@ xSemaphoreHandle x_semaphore;
 
 /**
  * @brief Bare metal programming with FreeRTOS example.
- * -
+ * - Binary semaphore example. As long as push button is pressed, the LED is
+ *   toggled by task 1 and a Semaphore is given. Semaphore is taken by task 2
+ *   or times out after 2 seconds with a corresponding message.
  * @return int
  */
 int main(void) {
@@ -51,6 +54,7 @@ int main(void) {
 
   // Initialize LED and user button.
   BspLedInit();
+  BspPbInit();
 
   // Initialize Console.
   BspConsoleInit();
@@ -82,23 +86,27 @@ int main(void) {
  */
 void vTask1(void* pv_parameters) {
   uint16_t count = 0;
+  // Initialize timing.
+  portTickType time_last_wakeup = xTaskGetTickCount();
 
   while (1) {
-    BspLedToggle();
-    ++count;
+    // Toggle LED only if user push button is pressed:
+    if (BspPbGetState()) {
+      BspLedToggle();
+      ++count;
+    }
 
     // Release semaphore every 10th count.
     if (count == 10) {
       // 'Give' semaphore, i.e. trigger Task 2. This yields to equal or higher
-      // priority
-      // tasks.
+      // priority tasks.
       xSemaphoreGive(x_semaphore);
 
       // Reset count (probably more efficient than using modulo).
       count = 0;
     }
-
-    vTaskDelay(100 / portTICK_PERIOD_MS);
+    // Wait for remainder of period.
+    vTaskDelayUntil(&time_last_wakeup, (100 / portTICK_PERIOD_MS));
   }
 }
 
@@ -108,6 +116,7 @@ void vTask1(void* pv_parameters) {
  * @param pv_parameters
  */
 void vTask2(void* pv_parameters) {
+  portBASE_TYPE status_semaphore;
   uint16_t count = 0;
 
   // Take the semaphore once to make sure it is empty.
@@ -118,9 +127,15 @@ void vTask2(void* pv_parameters) {
     // mechanism, therefore we don't need a delay function anymore.
     // Thus is asynchronously take semaphore. After printing it immediately
     // takes the semaphore again.
-    xSemaphoreTake(x_semaphore, portMAX_DELAY);
+    status_semaphore = xSemaphoreTake(x_semaphore, 2000);
 
-    stm32_printf("Semaphore taken %2dth time by task 2\r\n", count);
-    ++count;
+    if (status_semaphore == pdPASS) {
+      // Semaphore taken successfully.
+      stm32_printf("Semaphore taken %2dth time by task 2\r\n", count);
+      ++count;
+    } else {
+      // Semaphore timed out.
+      stm32_printf("Task_2 timed out waiting for semaphore.\r\n");
+    }
   }
 }
